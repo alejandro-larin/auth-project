5cbe58e5e5bf91bbcb02d8864250d5e213125ea5
Alex!?🍒 <alejandro.larin.orellana@gmail.com>
2026-01-09 19:30:11 +0100

    feat(auth_service): Adding business logic and add repository user

--- app/api/v1/auth/service.py
+++ app/api/v1/auth/service.py
@@ -1,11 +1,29 @@
 from .schemas import UserCreateRequest, UserResponse
+from .repository import UserRepository
+from .domain import UserRecord
+from typing import Optional
+from .security import hash_password
 
 class AuthService:
+    def __init__(self):
+        self._user_repo: UserRepository = UserRepository()
+
     def register_user(self, payload: UserCreateRequest) -> UserResponse:
 
+        existing_user: Optional[UserRecord] = (
+            self._user_repo.get_by_email(payload.email)
+        )
+
+
+        if existing_user:
+            raise ValueError("This user already exists")
+        
+        hashed: str = hash_password(payload.password)
+        user_record: UserRecord = self._user_repo.create(email=payload.email, password=hashed)
+
         return UserResponse(
-                id=1,                
-                email=payload.email
-            )
+            id=user_record["id"],
+            email=user_record["email"],
+        )
 
 auth_service = AuthService()
